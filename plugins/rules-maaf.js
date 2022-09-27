9ca1537ff74e8eff64eba41953a7a5118286cc70
malgtps12 <93052305+malgtps12@users.noreply.github.com>
2022-09-27 19:51:45 +0700

    Update rules-maaf.js

--- plugins/rules-maaf.js
+++ plugins/rules-maaf.js
@@ -4,7 +4,7 @@ let text = `${htki} RULES ${htka}
 > Minta maaf ke owner agar tidak terkena pelanggaran 
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Youtube', url: 'https://youtube.com/channel/UCACHvReRmw2fxgMutPFCBWg'}},
+    {index: 1, urlButton: {displayText: 'Youtube', url: '-'}},
 ]
 let tm = {
 text: text,
